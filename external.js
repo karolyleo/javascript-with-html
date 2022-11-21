e3a5b09070f1c08303a3135729ec451fcaaf372b
Leonard Karoly <leonard.a.karoly@gmail.com>
2022-11-21 11:29:02 -0600

    completed inline_js and external_js assignments

---
+++ external.js
@@ -0,0 +1,4 @@
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+color = prompt("What is your favorite color?");
+alert("Great, " + color + " is my favorite color too!")
